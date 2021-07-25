dff37df9d6e2752bab25dc7f9fe7a0ffeef4f7a1
aue-tripenko <67926726+san4ern@users.noreply.github.com>
2021-07-25 10:23:03 +0300

    Update EventsHandler.js

--- bot/Handlers/EventsHandler.js
+++ bot/Handlers/EventsHandler.js
@@ -1,17 +1,17 @@
 const { readdirSync } = require('fs');
 
 module.exports = async(client) => {
-    const eventsFiles = readdirSync('./bot/events')
+    const eventsFiles = readdirSync('./bot/Events')
         .filter(file => file.endsWith(".js"));
 
     if(!eventsFiles.length) return console.log('EVENTS   | No events were found');
 
     for (const file of eventsFiles) {
-        const event = require(`../events/${file}`);
+        const event = require(`../Events/${file}`);
         let eventName = file.split(".")[0];
 
         client.on(eventName, event.bind(null, client, eventName));
-        delete require.cache[require.resolve(`../events/${file}`)];
+        delete require.cache[require.resolve(`../Events/${file}`)];
 
         console.log(`EVENTS   | Loaded ${eventName} event`);
     }
